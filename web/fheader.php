0cc54e8a3241a17379f265f7ce68752283d4eaeb
Cam Parra <caeparra@gmail.com>
2017-05-06 17:10:05 -0600

    all done for week3 ... finally ;)

--- web/fheader.php
+++ web/fheader.php
@@ -8,9 +8,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.0.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="https://fastly.ink.sapo.pt/3.1.10/css/ink.css"> <!-- inks css file -->
+  <link rel="stylesheet" type="text/css" href="http://fastly.ink.sapo.pt/3.1.10/css/ink.css"> <!-- inks css file -->
   <link rel="stylesheet" href="fstyle.css">
-  <script type="text/javascript" src="https://fastly.ink.sapo.pt/3.1.10/js/ink-all.js"></script> <!-- inks js bundle -->
+  <script type="text/javascript" src="http://fastly.ink.sapo.pt/3.1.10/js/ink-all.js"></script> <!-- inks js bundle -->
 
   <title>Shirts N' Stuff</title>
 </head>
