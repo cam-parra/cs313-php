3396a8143acf08f64e247e367f1a816a8df0dc0f
Cam Parra <caeparra@gmail.com>
2017-05-06 17:21:48 -0600

    all done for week3 ... finally ;)

--- web/fheader.php
+++ web/fheader.php
@@ -4,13 +4,48 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.0.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="//fastly.ink.sapo.pt/3.1.10/css/ink.css"> <!-- inks css file -->
-  <link rel="stylesheet" href="fstyle.css">
-  <script type="text/javascript" src="//fastly.ink.sapo.pt/3.1.10/js/ink-all.js"></script> <!-- inks js bundle -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="">
+  <meta name="author" content="ink, cookbook, recipes">
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+  <!-- Place favicon.ico and apple-touch-icon(s) here  -->
+  <link rel="shortcut icon" href="./img/favicon.ico">
+  <link rel="apple-touch-icon" href="./img/touch-icon-iphone.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="./img/touch-icon-ipad.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="./img/touch-icon-iphone-retina.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="./img/touch-icon-ipad-retina.png">
+  <link rel="apple-touch-startup-image" href="./img/splash.320x460.png" media="screen and (min-device-width: 200px) and (max-device-width: 320px) and (orientation:portrait)">
+  <link rel="apple-touch-startup-image" href="./img/splash.768x1004.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)">
+  <link rel="apple-touch-startup-image" href="./img/splash.1024x748.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)">
+
+  <!-- load Ink's CSS -->
+  <link rel="stylesheet" type="text/css" href="./css/ink-flex.min.css">
+  <link rel="stylesheet" type="text/css" href="./css/font-awesome.min.css">
+
+  <!-- load Ink's CSS for IE8 -->
+  <!--[if lt IE 9 ]>
+      <link rel="stylesheet" href="../css/ink-ie.min.css" type="text/css" media="screen" title="no title" charset="utf-8">
+  <![endif]-->
+
+  <!-- test browser flexbox support and load legacy grid if unsupported -->
+  <script type="text/javascript" src="./js/modernizr.js"></script>
+  <script type="text/javascript">
+      Modernizr.load({
+        test: Modernizr.flexbox,
+        nope : './css/ink-legacy.min.css'
+      });
+  </script>
+
+  <!-- load Ink's javascript files -->
+  <script type="text/javascript" src="./js/holder.js"></script>
+  <script type="text/javascript" src="./js/ink-all.min.js"></script>
+  <script type="text/javascript" src="./js/autoload.js"></script>
+
 
   <title>Shirts N' Stuff</title>
 </head>
